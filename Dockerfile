be03a79367fb50480a08e2ff33faaea151c49965
sparkdea25 <michaeldaralola123@gmail.com>
2024-10-08 08:03:57 +0100

    update model

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,69 @@
-FROM python:3.9-slim
+# FROM python:3.9-slim
+# WORKDIR /app
 
-WORKDIR /app
+# RUN apt-get update && apt-get install -y \
+#     build-essential \
+#     curl \
+#     software-properties-common \
+#     git \
+#     && rm -rf /var/lib/apt/lists/*
 
-RUN apt-get update && apt-get install -y \
-    build-essential \
-    curl \
-    software-properties-common \
-    git \
-    && rm -rf /var/lib/apt/lists/*
+# # RUN git clone https://github.com/streamlit/streamlit-example.git .
 
-RUN git clone https://github.com/streamlit/streamlit-example.git .
+# RUN pip3 install -r requirements.txt
 
-RUN pip3 install -r requirements.txt
+# EXPOSE 8501
 
-EXPOSE 8501
+# HEALTHCHECK CMD curl --fail http://localhost:8501/_stcore/health
 
-HEALTHCHECK CMD curl --fail http://localhost:8501/_stcore/health
+# ENTRYPOINT ["streamlit", "run", "streamlit_app.py", "--server.port=8501", "--server.address=0.0.0.0"]
 
-ENTRYPOINT ["streamlit", "run", "streamlit_app.py", "--server.port=8501", "--server.address=0.0.0.0"]
+
+# FROM python:3.9.20-slim
+# WORKDIR /app
+
+# RUN pip install --no-binary=h5py h5py
+# COPY requirements.txt requirements.txt
+# COPY requirements.txt /app/
+# RUN pip install --upgrade pip
+
+# RUN pip3 install h5py
+# RUN pip install --upgrade setuptools 
+# RUN pip install -r requirements.txt
+# RUN usr/local/bin/python -m pip install --upgrade pip
+# RUN /Users/user/Documents/malaria-prediction/default_model/env/bin/python -m pip install --upgrade pip
+# RUN pip install --upgrade pip
+
+# RUN apt-get update && apt-get install -y libhdf5-dev
+
+# RUN HDF5_DIR=/opt/homebrew/Cellar/hdf5/1.14.3_1/  pip install --no-binary=h5py h5py
+# RUN  HDF5_VERSION=1.14.3_1 pip install --no-binary=h5py h5py
+# RUN CC="mpicc" HDF5_MPI="ON" HDF5_DIR=/opt/homebrew/Cellar/hdf5/1.14.3_1/  pip install --no-binary=h5py h5py
+
+
+# RUN pip install 
+# --no-cache-dir -r requirements.txt
+# RUN pip install
+# COPY . /app/
+# RUN pip install -r requirements.txt
+# COPY . .
+# EXPOSE 8080
+# # CMD ["python3", "main.py"]
+# CMD ["python", "main.py"]
+
+FROM python:3.12 
+
+RUN mkdir /usr/src/app
+
+COPY main.py /usr/src/app
+
+COPY requirements.txt /usr/src/app
+
+WORKDIR /usr/src/app
+
+RUN pip install -r requirements.txt
+
+CMD ["python", "main.py"]
+
+
+
---
+++ main.py
@@ -0,0 +1,103 @@
+from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
+import streamlit as st
+# from fastapi import FastAPI, Form, Depends, Request
+from flask import Flask, request, jsonify
+import logging
+import sys
+from twilio.rest import Client
+# from decouple import config
+import os
+
+# app = FastAPI()
+app = Flask(__name__)
+
+
+account_sid = os.environ.get('TWILIO_ACCOUNT_SID')
+# config("TWILIO_ACCOUNT_SID")
+auth_token = os.environ.get('TWILIO_AUTH_TOKEN')
+# config("TWILIO_AUTH_TOKEN")
+client = Client(account_sid, auth_token)
+twilio_number = os.environ.get("TWILIO_NUMBER")
+# config('TWILIO_NUMBER')
+
+documents = SimpleDirectoryReader("data").load_data()
+index = VectorStoreIndex.from_documents(documents)
+
+query_engine = index.as_query_engine()
+response = query_engine.query("How does SRS Solar work?")
+
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+logging.getLogger().addHandler(logging.StreamHandler(stream=sys.stdout))
+
+def append_interaction_to_chat_log(question, answer, chat_log=None):
+    if chat_log is None:
+        chat_log = start_chat_log
+    return f'{chat_log}Human: {question}\nAI: {answer}\n'
+
+
+# Sending message logic through Twilio Messaging API
+def send_message(to_number, body_text):
+    try:
+        message = client.messages.create(
+            from_=f"whatsapp:{twilio_number}",
+            body=body_text,
+            to=f"whatsapp:{to_number}"
+            )
+        logger.info(f"Message sent to {to_number}: {message.body}")
+    except Exception as e:
+        logger.error(f"Error sending message to {to_number}: {e}")
+        
+        
+@app.route('/message', methods=['POST'])
+def bot():
+    incoming_msg = request.values['Body']
+    phone_number = (request.values['WaId'])
+
+    if incoming_msg:
+        chat_log = session.get('chat_log')
+        answer = query_engine.query(incoming_msg)
+        session['chat_log'] = append_interaction_to_chat_log(incoming_msg, answer, chat_log)
+        sendMessage(answer, phone_number)
+        print(answer)
+    else:
+        sendMessage("Message Cannot Be Empty!")
+        print("Message Is Empty")
+    r = MessagingResponse()
+    r.message("")        
+    return str(r)
+
+
+host="0.0.0.0"
+port=8002
+app_name="app.ma:app"
+
+# if __name__ == '__starter__':
+#     uvicorn.run(app_name, host=host, port=port)
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=8000, debug=True)
+
+
+# if __name__ == '__main__':
+#     app.run()
+        
+
+# # Streamlit app
+# st.title("SATORI BOT")
+
+# # User input
+# user_input = st.text_input("Enter your message:", "")
+
+# # Button to trigger response
+# if st.button("Send"):
+#     if user_input:
+#         with st.spinner("Processing..."):
+#             try:
+#                 # Get the response from OpenAI
+#                 response = query_engine.query(user_input)
+#                 st.success(response)
+#             except Exception as e:
+#                 st.error(f"Error: {e}")
+#     else:
+#         st.warning("Please enter a message.")
+# print(response)
